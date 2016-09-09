fd86404d49ca2967e4eefc3da6223b6b519d9efa
hazrmard <ibrahim78786@gmail.com>
2016-09-09 14:39:32 -0500

    added ship + fixed controls

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -3,21 +3,27 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
 
-	public int num;
+	public int num;                                                     // # of obstacles
 	public float maxSpeed;
-    public float acceleration;
-    public float deceleration;
+    public float sensitivity;                                           // speed of rotations
+    public float acceleration;                                          // ship acceleration
+    public float deceleration;                                          // ship deceleration
+
     private Obstacle[] spheres;
-    private float rotationX = 0F;
+    private Transform ship;
+    private float rotationX = 0F;                                       // Global ship orientation angles
     private float rotationY = 0F;
-    private Quaternion originalRot;
+    private float rotationZ = 0F;
+    private float tiltRotZ = 0F;                                        // Local ship tilt angles 
+    private float tiltRotY = 0F;
+    private float tiltRotX = 0F;
     private Vector3 accelDir = Vector3.zero;
     private Vector3 velocity = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-        originalRot = transform.localRotation;
         spheres = new Obstacle[num];
+        ship = transform.Find("Ship");
 
 		int i;
 		for (i = 0; i < num; i++) {
@@ -29,39 +35,83 @@ public class Main : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         accelDir = Vector3.zero;
-		if (Input.GetKey (KeyCode.W))
+		if (Input.GetKey (KeyCode.W))                                   // W KEY
         {
             accelDir.z = 1;
 		}
-        else if (Input.GetKey (KeyCode.S))
+        else if (Input.GetKey (KeyCode.S))                              // S KEY
         {
 			accelDir.z = -1;
 		}
-        if (Input.GetKey (KeyCode.Space))
+        if (Input.GetKey (KeyCode.Space))                               // SPACEBAR
         {
             accelDir.y = 1;
         }
-        else if (Input.GetKey(KeyCode.LeftAlt))
+        else if (Input.GetKey(KeyCode.LeftAlt))                         // L ALT KEY
         {
             accelDir.y = -1;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))                                    // D KEY
         {
-            accelDir.x = 1;
+            //accelDir.x = 1;
+            rotationY = maxSpeed * sensitivity * Time.deltaTime;
+            tiltRotY += (tiltRotY + 15F) * Time.deltaTime;
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))                               // A KEY
         {
-            accelDir.x = -1;
+            //accelDir.x = -1;
+            rotationY = - maxSpeed * sensitivity * Time.deltaTime;
+            tiltRotY -= (15F - tiltRotY) * Time.deltaTime;
         }
+        else
+        {
+            tiltRotY -= tiltRotY * deceleration * Time.deltaTime;
+            rotationY -= rotationY * deceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))                           // RIGHT ARROW
+        {
+            rotationZ = - maxSpeed * sensitivity * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))                       // LEFT ARROW
+        {
+            rotationZ = maxSpeed * sensitivity * Time.deltaTime;
+        }
+        else
+        {
+            rotationZ -= rotationZ * deceleration * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))                              // UP ARROW
+        {
+            rotationX = maxSpeed * sensitivity * Time.deltaTime;
+            tiltRotX += (tiltRotX + 15F) * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))                       // DOWN ARROW
+        {
+            rotationX = - maxSpeed * sensitivity * Time.deltaTime;
+            tiltRotX -= (15F - tiltRotX) * Time.deltaTime;
+        }
+        else
+        {
+            tiltRotX -= tiltRotX * deceleration * Time.deltaTime;
+            rotationX -= rotationX * deceleration * Time.deltaTime;
+        }
+
+        rotationX %= 360;
+        rotationY %= 360;
+        rotationZ %= 360;
+
+        tiltRotZ = Mathf.Clamp(tiltRotZ, -15F, 15F);
+        tiltRotY = Mathf.Clamp(tiltRotY, -15F, 15F);
+        tiltRotX = Mathf.Clamp(tiltRotX, -15F, 15F);
+        // Ship tilting effects
+        Quaternion qTZ = Quaternion.AngleAxis(tiltRotZ, Vector3.forward);
+        Quaternion qTY = Quaternion.AngleAxis(tiltRotY, Vector3.up);
+        Quaternion qTX = Quaternion.AngleAxis(tiltRotX, Vector3.right);
+
+        transform.Rotate(rotationX, rotationY, rotationZ);
+        ship.localRotation = qTX * qTY * qTZ;
 
         accelerate();
-
-        rotationX += Input.GetAxis("Mouse X") * maxSpeed;
-        rotationY += Input.GetAxis("Mouse Y") * maxSpeed;
-        rotationY = Mathf.Clamp(rotationY, -90F, 90F);
-        Quaternion qX = Quaternion.AngleAxis(rotationX, Vector3.up);
-        Quaternion qY = Quaternion.AngleAxis(rotationY, Vector3.left);
-        transform.localRotation = originalRot * qX * qY;
     }
 
 
